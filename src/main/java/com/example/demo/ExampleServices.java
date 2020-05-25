fea276cf835c4371a40fd6e4902345ae044309cd
Rustam Minibaev <Rustam.Minibaev@magenta-technology.com>
2020-05-25 16:12:29 +0400

    Initial commit

---
+++ src/main/java/com/example/demo/ExampleServices.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class ExampleServices {
+    public static void example(MyBean bodyIn) {
+        bodyIn.setName( "Hello, " + bodyIn.getName() );
+        bodyIn.setId(bodyIn.getId()*10);
+    }
+}
